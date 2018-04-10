8050d2ffd4cc5d10f13d3cfd6ec03fdebb1f73ea
Justine <justine.santot@gmail.com>
2018-04-10 13:52:01 +0200

    cours4

--- cours4/script.js
+++ cours4/script.js
@@ -4,5 +4,23 @@ $( document ).ready(function() {
    $("#welcome").html('<h4>Bienvenue, <br> soyez pret à renouveler de nouveaux défis !</h4>');
    
 };
+
 welcome();
+
+function getUsername() {
+    //affiche une boite de dialogue 
+    var  user = prompt("Veuillez rentrez votre nom:", "");
+    // la fonction se termine en retournant le nom récupéré par la boite de dialogue
+    return user
+}
+getUsername() ;
+
+function welcome() {
+    var name= getUsername();
+    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez pret à renouveler de nouveaux défis ! </h4>');
+
+};
+// Appel la fonction
+welcome();
+
 })
