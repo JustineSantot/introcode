eadf976632d2b5301bb1b36e22f494e6a0eb1814
Justine <justine.santot@gmail.com>
2018-04-23 20:47:35 +0200

    video link

--- cours4/script.js
+++ cours4/script.js
@@ -39,13 +39,14 @@ function showChallenge(liste) {
   var data="";
 
   for ( var i=0; i < liste.length; i++) {
+
     data+='<div class="defi">';
 
     var challenge=liste[i];
 
     data+='<h2>'+challenge.nom+'</h2>';
     data+='<p>'+challenge.description+'</p>';
-    data+='<iframe width="364" height="204" src="+challenge.youtube+" frameborder="0" allowfullscreen=""></iframe>'
+    data+='<iframe width="364" height="204" src="'+challenge.youtube+'" frameborder="0" allowfullscreen=""></iframe>'
     data+="</div>";
   }
   $("#defis").html(data);
