fdb6228dcd8d1c9811cf2b6c997706e7f533a5a0
Justine <justine.santot@gmail.com>
2018-04-24 11:29:07 +0200

    css amelioration

--- cours4/script.js
+++ cours4/script.js
@@ -1,24 +1,26 @@
 $( document ).ready(function() {
 
   function welcome() {
-   $("#welcome").html('<h4>Bienvenue, <br> soyez pret à renouveler de nouveaux défis !</h4>');
+   $("#welcome").html('<h4>Bienvenue, <br> sois pret à renouveler tes défis !</h4>');
    
 };
 
-welcome();
 
 function getUsername() {
     //affiche une boite de dialogue 
-    var  user = prompt("Veuillez rentrez votre nom:", "");
+    var  user = prompt("Rentre ton nom pour accéder à de nouveaux défis:", "");
     // la fonction se termine en retournant le nom récupéré par la boite de dialogue
     return user
 }
 
 function welcome() {
     var name= getUsername();
-    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez pret à renouveler de nouveaux défis ! </h4>');
+    $("#welcome").html('<h4>Bienvenue '+name+', <br> sois pret à renouveler tes défis ! </h4>');
 
 };
+
+// Appel la fonction
+welcome();
 
 function loadChallenges() {
   $.ajax(
